ebe282b023395b1dce444fd6085f5f75a6444343
CatBro <karangoel59@gmail.com>
2019-02-27 19:52:12 +0530

    middleware

--- app.js
+++ app.js
@@ -3,6 +3,17 @@ var app = express();
 
 var port = process.env.PORT || 3000;
 
+app.use(function(req,res,next){
+    console.log("First");
+    next();
+});
+//req.querry.color
+//url/?color=any
+app.use(function (req, res, next) {
+    console.log("Second");
+    next();
+});
+
 app.listen(port,function(){
     console.log("listening on "+port);
 });
