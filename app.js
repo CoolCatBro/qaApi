b79336802343ec372014d27e7aa4d3caca8e761b
CatBro <karangoel59@gmail.com>
2019-02-27 23:12:17 +0530

    logger and sql

--- app.js
+++ app.js
@@ -7,11 +7,30 @@ var routes = require('./routes');
 
 //Use json bodypraser
 var jsonParser = require('body-parser').json;
+var logger = require("morgan");
+
+app.use(logger('dev'));
 app.use(jsonParser());
 
 //Middlewares handle by routes module
 app.use('/questions',routes);
 
+//catch 404 and foward to error handler
+app.use(function(req,res,next){
+    var err = new Error("Not Found");
+    err.status = 404;
+    next(err);
+});
+
+// Error Handler
+app.use(function(err,req,res,next){
+    res.status(err.status || 500);
+    res.json({
+        error: {
+            message: err.message
+        }
+    });
+});
 
 //Listening on port 3000
 var port = process.env.PORT || 3000;
--- routes.js
+++ routes.js
@@ -4,18 +4,57 @@ var express = require('express');
 var router = express.Router();
 
 //GET /questions
-router.get('/:id',function(req,res){
-    //return all the questions
-    res.json({response: "GET request", id: req.params.id});
+//return all questions
+router.get('/',function(req,res){
+    res.json({response: 'GET request'});
 });
 
 //POST /questions
-router.post('/', function (req, res) {
-    //return all the questions
+//submit a question
+router.post('/:qID', function (req, res) {
+    //post an answer
+});
+
+//GET /answers
+//return answers of a question
+router.get('/:qID', function (req, res) {
+});
+
+//POST /answer
+//submit answer
+router.post('/:qID/answers', function (req, res) {
+        res.json({
+            response: 'post request'
+        });
+});
+
+//PUT /answer
+//update a answer
+router.put('/:qID/answers/:aID',function(req,res){
+});
+
+//DELETE /answer
+//delete a answer
+router.delete('/:qID/answers/:aID', function (req, res) {
+});
+
+//vote for /answer
+//upvote or downvote a answer
+router.post('/:qID/answers/:aID/vote-:dir',
+function(req,res,next){
+    if(req.params.dir.search(/\b(up|down)\b/) === -1){
+        var err = new Error("Not Found");
+        err.status = 404;
+        next(err);
+    } else {
+        next();
+    }
+},
+function(req,res){
     res.json({
-        response: "GET request",
-        body: req.body
+        response: 'post request'
     });
 });
+
 
 module.exports = router;
