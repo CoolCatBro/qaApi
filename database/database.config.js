1154b07b3810450f44349574923a9db2f4c15ab8
Kg <karangoel59@gmail.com>
2019-03-01 19:40:44 +0530

    Update database.config.js

--- database/database.config.js
+++ database/database.config.js
@@ -1,6 +1,6 @@
 'use strict';
 
-//The project will be onlu using low level driver for my sql.
+//The project will be only using low level driver for my sql.
 //ORM can also be implemented for scalbilty purpose
 
 //The config of database and table
